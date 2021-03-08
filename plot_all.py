14f8a34f3ed967a03c58ad7350d842cff923fd25
rreinecke <reinecke@em.uni-frankfurt.de>
2021-03-08 15:49:50 +0100

    provides now outputs for all questions -< base to fill notebook and explore additonal questions

--- plot_all.py
+++ plot_all.py
@@ -121,7 +121,7 @@ def p_opinion(data):
     Plot Opinions
     '''
 
-    names = {'O101':"Agreement",'O102':"Reproduce?",'O103':"Reasons"}   
+    names = {'O101':"Agreement",'O102':"Reproduce?",'O103':"Reasons", 'S201': "Actions"}   
     
     #iterate questions of catergory and plot box for each
     for q in names.keys():
@@ -131,7 +131,7 @@ def p_opinion(data):
         d.reset_index(level=0, inplace=True) 
         df = pd.melt(d, id_vars=['index'], value_vars=cols)
 
-        res = get_label_by_names(data, cols, [" Opinion:", "reasons:", "Reproduce?:", "((", explenation_s, explenation_a_s, expl_missing_s, expl_soft_s])
+        res = get_label_by_names(data, cols, [" Opinion:", "reasons:", "Reproduce?:", "Helpful Suggestions:", "((", explenation_s, explenation_a_s, expl_missing_s, expl_soft_s])
         df["variable"] = df["variable"].map(res)
 
 
@@ -161,30 +161,111 @@ def p_self(data):
     Plot Self-assemssment
     '''
 
-
-    d = data['data'][names.keys()]
+    names = {'S103': "Frequence Software", 'S110': "Frequence coding", 'S202': 'Ownership', 'S113': "New student", 'S204': "Community - Full text answer"} 
+    
     #iterate questions of catergory and plot box for each
     for q in names.keys():
-        date = d[q].copy().to_frame()
-        # What should the Y-axis say?
-        date["Variable"] = names[q]
-        
-        # Get the full answer text
-        an = data['values']
-        an = an[an['VAR'] == q]
-        #RESPONSE MEANING
-        date = date.merge(an, left_on=q, right_on='RESPONSE')
-        
-        ax = sns.boxplot(y="Variable", x=q, data=date, orient="h")
-        ax.set_xticklabels(date["MEANING"],rotation=60)
-        ax.figure.savefig(d_path + sl + q + ".png")
+        d, cols = get_all_data(data, q)
+
+        if q == "S204":
+            d.to_csv("S204_answers.csv", index=False)
+            continue
+
+        res = get_full_response(data, q)
+        d[q] = d[q].map(res)
+
+        ax = sns.histplot(x=q, data=d, discrete=True)
+        plt.xticks(rotation=90, fontsize =5)
+        plt.subplots_adjust(bottom=.4)
+
+
+        ax.figure.savefig(d_path + sl + q + ".png", dpi=200)
         ax.figure.clf()
+
+def p_self2(data):
+    names = {'S111': "Which licences you use", 'S112': "Licences you know", 'S101': "What languages", 'S104': "Methods and concepts", 'S105': "Tools", 'S203': "What keeps you",'S106': "Learning"}
+
+    def count_occur(data, string, what):
+        return data.loc[data[string] == what][string].count()
+    
+    for q in names.keys():
+        d, cols = get_all_data(data, q)
+        
+        if q == "S111":
+            print("What licences do you use?")
+            print(d["S111s"].unique())
+            
+        if q == "S112":
+            print("Which of the licences are you familar with?")
+            un = d['S112_08a'].unique()
+            #FIXME currently this does not assess how often an alternative was mentioned
+            print("Other licences mentioned: {}".format(un))
+            print(d.mean())
+            print("GNU is number 4")
+            print("Total responses: {}".format(d["S112"].count()))
+            counts = {"I dont know them": count_occur(d, "S112", -2), "Only by name": count_occur(d, "S112", -1)}
+            print(counts)
+
+        if q == "S101":
+            print("What kind of programming languages do you mainly use?")
+            un = d['S101_06a'].unique()
+            #FIXME currently this does not assess how often an alternative was mentioned
+            print("Other languages mentioned: {}".format(un))
+            print(d.mean())
+            print("03= Python, 04= R")
+            print("Total responses: {}".format(d["S101"].count()))
+            counts = {"None of the above": count_occur(d, "S101", -1)}
+            print(counts)
+        
+        if q == "S104":
+            print("What methods are you applying?")
+            un = d['S104_09a'].unique()
+            #FIXME currently this does not assess how often an alternative was mentioned
+            print("Others mentioned: {}".format(un))
+            print(d.mean())
+            print("04 = Object oriented")
+            print("Total responses: {}".format(d["S104"].count()))
+            counts = {"None of the above": count_occur(d, "S104", -1)}
+            print(counts)
+
+        if q == "S105":
+            print("What tools are you using?")
+            un = d['S105_04a'].unique()
+            #FIXME currently this does not assess how often an alternative was mentioned
+            print("Others mentioned: {}".format(un))
+            print(d.mean())
+            print("01 = Version control")
+            print("Total responses: {}".format(d["S105"].count()))
+            counts = {"None of the above": count_occur(d, "S105", -1)}
+            print(counts)
+
+        if q == "S106":
+            print("Where did you learn to programm?")
+            print(d.mean())
+            print("...")
+            print("Total responses: {}".format(d["S106"].count()))
+            counts = {"I'm not able to write my own code": count_occur(d, "S106", -1)}
+            print(counts)
+
+        if q == "S203":
+            print("What keeps you from Open Source?")
+            un = d['S203_06a'].unique()
+            #FIXME currently this does not assess how often an alternative was mentioned
+            print("Others mentioned: {}".format(un))
+
+            print(d.mean())
+            print("...")
+            print("Total responses: {}".format(d["S203"].count()))
+            counts = {"I publish all my code as open source": count_occur(d, "S203", -1), "I don't want to": count_occur(d, "S203", -2), "Does not apply to me": count_occur(d, "S203", -3)}
+            print(counts)
+
 
 
 def all():
     data = read_data()
-    p_demo(data)
-    p_opinion(data)
+    #p_demo(data)
+    #p_opinion(data)
     #p_self(data)
+    p_self2(data)
 
 all()
