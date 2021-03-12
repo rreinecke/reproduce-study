00fafdf854a7745bf0e03fac1236417efd7318de
rreinecke <reinecke@em.uni-frankfurt.de>
2021-03-12 17:23:26 +0100

    simple word cloud script

---
+++ plot_cloud.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+
+with open('S204_answers.csv', 'r') as file:
+    text = file.read().replace('\n', '')
+
+fig = plt.figure(figsize=(40,30))
+plt.axis("off")
+wc = WordCloud(width=3000, height= 2000, random_state=1, background_color='black', colormap='Pastel1', collocations=False, stopwords=STOPWORDS).generate(text)
+plt.imshow(wc)
+fig.savefig('a.png', bbox_inches='tight')
