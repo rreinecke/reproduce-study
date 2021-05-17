f03072be8b5dd02fdeb581a519a259af581729fa
rreinecke <robert.reinecke@uni-potsdam.de>
2021-05-17 10:33:19 +0200

    information if latex tool is executed without figures

--- report/build.sh
+++ report/build.sh
@@ -1,6 +1,9 @@
 # remove old tmp files
 rm *.aux
 rm *.log
+
+[ "$(ls -A ../figs)" ] && echo "Found figures" || echo "Figures not found! Run the plotting script first"
+
 pdflatex data_report.tex
 # build twice to make sure all references are ok
 pdflatex data_report.tex
