2b2a5235adb25fae82f418b54ae45fda4b6fb4e3
rreinecke <robert.reinecke@uni-potsdam.de>
2021-05-17 10:50:38 +0200

    ups remove commented lines

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ import plot_all
 import os
 
 if __name__ == '__main__':
-   #process_data.process()
-   #plot_all.all()
+   process_data.process()
+   plot_all.all()
    print("Creating latex report from figures")
    os.system("cd report && ./build.sh")
