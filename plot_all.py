06dd532a0dc9b8fb03c96286f284476ec7a4a003
Tim Trautmann <t.trautmann@em.uni-frankfurt.de>
2021-05-19 14:32:06 +0200

    IMP: calculate dont know answer ratio

--- plot_all.py
+++ plot_all.py
@@ -205,6 +205,12 @@ def p_opinion(data):
     #iterate questions of catergory and plot box for each
     for q in names.keys():
         df = get_opinion(data,q)
+        # calculate the percentage of dont know answers (-1)
+        valid_answers_n = df[df.value > 0].groupby('variable').count()
+        dontknow_answers_n = df[df.value < 0].groupby('variable').count()
+        answers_n_categories = pd.merge(valid_answers_n.value, dontknow_answers_n.value, how="outer",
+                                        left_index=True, right_index=True).fillna(0)
+        dont_know_ratio = answers_n_categories.value_y / answers_n_categories.value_x
         if q == "O102": 
             plt.figure(figsize=(12,4))
             plt.yticks(fontsize = 8)
@@ -217,7 +223,7 @@ def p_opinion(data):
             continue
 
         plt.figure(figsize=(12,4))
-        ax = sns.boxplot(y="variable", x="value", data=df, orient="h")
+        ax = sns.boxplot(y="variable", x="value", data=df[df.value > 0], orient="h")
 
         plt.subplots_adjust(left=.6)
         plt.yticks(fontsize = 7)
